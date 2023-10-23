93d8a6ae48451d51121f1d777dbaae55558b50eb
Felix Widell <felix.widell@gmail.com>
2023-10-23 21:14:37 +0900

    Add project files.

---
+++ Lab5_GrundernaOOP/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_GrundernaOOP
+{
+    internal class Circle
+    {
+        private double _radius;
+
+        public double Radius
+        {
+            get => _radius; 
+            set => _radius = value;
+        }
+
+        public double GetArea()
+        {
+            return _radius * _radius * Math.PI;
+        }
+
+        public double GetCircumference()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public double GetSphereVolume()
+        {
+            return (4 / 3) * Math.PI * (Radius * Radius * Radius);
+        }
+    }
+}
---
+++ Lab5_GrundernaOOP/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab5_GrundernaOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var newCircle = new Circle();
+            newCircle.Radius = 5;
+            Console.WriteLine("Cirkel med radie 5 har arean: " + newCircle.GetArea().ToString("#.00"));
+            newCircle.Radius = 6;
+            Console.WriteLine("Cirkel med radie 6 har arean: " + newCircle.GetArea().ToString("#.00"));
+            Console.WriteLine("Cirkel med radie 6 har Omkretsen: " + newCircle.GetCircumference().ToString("#.00"));
+            Console.WriteLine("Glob med radie 6 har Volymen: " + newCircle.GetSphereVolume().ToString("#.00"));
+        }
+    }
+}
